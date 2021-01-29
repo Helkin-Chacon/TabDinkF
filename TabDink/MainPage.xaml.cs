77fe392fac165da84b8892b28e36a8fa60579b78
Helkin-Chacon <63528430+Helkin-Chacon@users.noreply.github.com>
2021-01-28 21:12:45 -0500

    subida!

---
+++ TabDink/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using Syncfusion.SfNavigationDrawer.XForms;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TabDink
+{
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            
+            bt.Clicked += (object sender, System.EventArgs e) =>
+            {
+
+                SfNavigationDrawer hola = navigationDrawer1;
+
+                hola.ToggleDrawer();
+
+                simTab.SelectedIndex = 4;
+
+
+            };
+        }
+
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            DisplayAlert("Message", "CenterButton Clicked", "Ok");
+        }
+
+        private void TabView_CenterButtonTapped(object sender, EventArgs e)
+        {
+            DisplayAlert("Message", "CenterButton Clicked", "Ok");
+        }
+    }
+
+    public class ContactInfo
+    {
+        public string Name { get; set; }
+        public long Number { get; set; }
+    }
+
+    public class ContactsViewModel
+    {
+        private ObservableCollection<ContactInfo> contactList;
+        public ObservableCollection<ContactInfo> ContactList
+        {
+            get { return contactList; }
+            set { contactList = value; }
+        }
+
+        public ContactsViewModel()
+        {
+            ContactList = new ObservableCollection<ContactInfo>();
+            ContactList.Add(new ContactInfo { Name = "Aaron", Number = 7363750 });
+            ContactList.Add(new ContactInfo { Name = "Adam", Number = 7323250 });
+            ContactList.Add(new ContactInfo { Name = "Adrian", Number = 7239121 });
+            ContactList.Add(new ContactInfo { Name = "Alwin", Number = 2329823 });
+            ContactList.Add(new ContactInfo { Name = "Alex", Number = 8013481 });
+            ContactList.Add(new ContactInfo { Name = "Alexander", Number = 7872329 });
+            ContactList.Add(new ContactInfo { Name = "Barry", Number = 7317750 });
+            ContactList.Add(new ContactInfo { Name = "Blexo", Number = 7363750 });
+            ContactList.Add(new ContactInfo { Name = "Cassey", Number = 7323250 });
+            ContactList.Add(new ContactInfo { Name = "Danny", Number = 7239121 });
+        }
+    }
+}
---
+++ TabDink/Utils/FDicons.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TabDink.Utils
+{
+	public static class FDicons
+	{
+
+		public static string Home = "\uE800";
+		public static string Academy = "\uE804";
+		public static string Category = "\uE801";
+		public static string Profile = "\uE803";
+	}
+}
